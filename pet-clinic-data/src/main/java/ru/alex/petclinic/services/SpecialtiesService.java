e379afe6dbab8bcf01212d012708149c2f91abbb
dagon1093 <P0werofgood>
2021-09-30 08:25:54 +0300

    Added SpecialtyService and SpecialityMapService. Close #31

---
+++ pet-clinic-data/src/main/java/ru/alex/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package ru.alex.petclinic.services;
+
+import ru.alex.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/ru/alex/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package ru.alex.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import ru.alex.petclinic.model.Speciality;
+import ru.alex.petclinic.services.SpecialtiesService;
+
+import java.util.Set;
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
