e28e632f3f14f71bc6b416e6a999424236be3032
dagon1093 <P0werofgood>
2021-10-02 10:15:20 +0300

    Fixed broken links. Close #45

--- pet-clinic-web/src/main/java/ru/alex/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/ru/alex/petclinic/controllers/IndexController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-
     @RequestMapping({"/","","index","index.html"})
     public String index(){
         return "index.html";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler(){
+        return "notImplemented";
     }
 }
